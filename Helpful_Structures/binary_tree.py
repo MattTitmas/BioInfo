4f24b83897c7b2e01364ebf146853e0e0c91a5cd
MattTitmas <mct52@cam.ac.uk>
2022-10-28 17:55:23 +0100

    feat: add Base Class

---
+++ Helpful_Structures/binary_tree.py
@@ -0,0 +1,64 @@
+from typing import TypeVar
+
+binary_type = TypeVar('binary_type')
+
+
+class BinaryTree:
+    def __init__(self, value: binary_type):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def display(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = f'{self.value}'
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left.display()
+            s = f'{self.value}'
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right.display()
+            s = f'{self.value}'
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left.display()
+        right, m, q, y = self.right.display()
+        s = f'{self.value}'
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    def __str__(self):
+        lines, *_ = self.display()
+        to_return = ""
+        for line in lines:
+            to_return += f'{line}\n'
+        return to_return[:-1]
+
+
