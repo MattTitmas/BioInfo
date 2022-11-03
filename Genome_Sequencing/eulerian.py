d9c11a4df6a8d80bc4408e4f15ebb7bbb1cf20b2
MattTitmas <mct52@cam.ac.uk>
2022-11-03 16:49:04 +0000

    feat: make it so the graph doesn't have to be cyclic

--- Genome_Sequencing/eulerian.py
+++ Genome_Sequencing/eulerian.py
@@ -10,26 +10,51 @@ def find_eulerian(g: Dict[str, List[str]]):
             unused_edges.append((key, value))
             edges_in[value] = edges_in.get(value, 0) + 1
 
-    proper_graph = all([edges_in[key] == edges_out[key] for key in g])
-    assert proper_graph is True, 'All nodes in the graph should have the same number of edges in as edges out'
+    start_found, end_found = False, False
+    two_starts, two_ends = False, False
+    start, end = '', ''
+    proper_graph = []
+    for key in set(edges_in.keys()).union(set(edges_out.keys())):
+        equal = edges_in.get(key, 0) == edges_out.get(key, 0)
+        proper_graph.append(equal)
+        if not equal:
+            if edges_in.get(key, 0) == 0:
+                start = key
+                if start_found:
+                    # Multiple end nodes is bad, can't start at all of them
+                    two_starts = True
+                start_found = True
+            if edges_out.get(key, 0) == 0:
+                end = key
+                if end_found:
+                    # Multiple end nodes is bad, can't reach all of them
+                    two_ends = True
+                end_found = True
 
+    if (not all(proper_graph)) and not (start_found and end_found and (not two_starts) and (not two_ends)):
+        assert proper_graph is True, 'All nodes in the graph should have the same number of edges in as edges out, ' \
+                                     'except the start and end node '
+
+    if start_found and end_found:
+        # Add a connection from the end of the graph to the beginning, to make the graph cyclic
+        g[end] = [start]
+        unused_edges.append((end, start))
 
     order = []
-    seen_nodes = ['A']
+    seen_nodes = [list(g.keys())[0]]
 
     while len(unused_edges) > 0:
         starting_node, next_node = unused_edges.pop(0)
         if starting_node not in seen_nodes:
             unused_edges.append((starting_node, next_node))
             continue
-
         # Get position in  {order} in which to insert this sub loop
         insert_position = min([order.index(i) for i in order if i[1] == starting_node], default=-2) + 1
         order.insert(insert_position, (starting_node, next_node))
 
         # Update insert_position (But not if inserting at the end of the list)
         insert_position += (1 if insert_position > 0 else 0)
-        while next_node != starting_node:
+        while next_node != starting_node or g.get(next_node, -1) == -1:
             seen_nodes.append(next_node)
             prev_node = next_node
             next_node = g[next_node].pop(0)
@@ -38,19 +63,26 @@ def find_eulerian(g: Dict[str, List[str]]):
 
             # Update insert_position (But not if inserting at the end of the list)
             insert_position += (1 if insert_position > 0 else 0)
-    return order
+    order.insert(0, order[-1])
+
+    if start_found and end_found:
+        # Remove the last edge, as this connects the end of the graph to be the beginning
+        return order[:-2]
+    return order[:-1]
 
 
 def main():
     directed_graph = {
-        'A': ['B'],
-        'B': ['C', 'F'],
-        'C': ['D', 'F'],
-        'D': ['A', 'G'],
-        'E': ['C', 'D'],
-        'F': ['E', 'H'],
-        'G': ['E'],
-        'H': ['B']
+        'TA': ['AA'],
+        'AA': ['AT'],
+        'AT': ['TG', 'TG', 'TG'],
+        'TG': ['GC', 'GG', 'GT'],
+        'GC': ['CC'],
+        'CC': ['CA'],
+        'CA': ['AT'],
+        'GG': ['GG', 'GA'],
+        'GA': ['AT'],
+        'GT': ['TT'],
     }
 
     eulerian = find_eulerian(directed_graph)
